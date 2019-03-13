2db4229ec61bf86393addf499a5f288cd42b324b
Roman Sergienko <roman.sergienko@taskdata.com>
2019-03-13 22:00:45 +0400

    spring boot carcass

---
+++ src/main/java/ru/sergienko/SpringApplication.java
@@ -0,0 +1,11 @@
+package ru.sergienko;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApplication {
+
+  public static void main(String[] args) {
+    org.springframework.boot.SpringApplication.run(SpringApplication.class, args);
+  }
+}
